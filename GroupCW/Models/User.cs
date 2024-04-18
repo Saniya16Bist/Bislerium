7e4a152b06c143b39522b0a0a2e40a5593c43c1d
Saniya <131156300+Saniya16Bist@users.noreply.github.com>
2024-04-18 19:55:53 +0545

    Add files via upload

---
+++ GroupCW/Models/User.cs
@@ -0,0 +1,29 @@
+﻿using System.Runtime.InteropServices;
+
+namespace GroupCW.models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }  // Consider using more secure methods for password storage
+        // Additional user properties
+    }
+
+    public class Blog
+    {
+        public int BlogId { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        // Additional blog properties
+    }
+
+    public class Comment
+    {
+        public int CommentId { get; set; }
+        public string Text { get; set; }
+        public int BlogId { get; set; }  // Foreign key from Blog
+        // Additional comment properties
+    }
+}
+
---
+++ GroupCW/data/MyBloggingApp.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Reflection.Metadata;
+using GroupCW.models;
+
+namespace GroupCW.data
+{
+    public class MyBloggingApp
+    {
+        public DbSet<User>? Users { get; set; }
+        public DbSet<Blog>? Blogs { get; set; }
+        public DbSet<Comment>? Comments { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=bloggingapp.db");
+        }
+    }
+} 
